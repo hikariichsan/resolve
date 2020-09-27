41c87c8b8b42c962296415706bec7d49d01b51d1
khairulichsan <kichsan92@gmail.com>
2020-09-28 04:06:02 +0700

    save

--- src/routes/developer.js
+++ src/routes/developer.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 const {registerDeveloper,loginDeveloper,getDataDeveloper, deleteDeveloper, putDeveloper, patchDeveloper} = require('../controllers/developer')
-
+const {authorizationDev} = require('../middleware/auth')
 const router = Router()
 
 router.post('/register', registerDeveloper)
 router.post('/login', loginDeveloper)
 router.get('/', getDataDeveloper)
-router.put('/:id', putDeveloper)
-router.delete('/:id', deleteDeveloper)
-router.patch('/:id', patchDeveloper)
+router.put('/:id',authorizationDev, putDeveloper)
+router.delete('/:id',authorizationDev, deleteDeveloper)
+router.patch('/:id',authorizationDev, patchDeveloper)
 
 module.exports= router
--- src/routes/recruiter.js
+++ src/routes/recruiter.js
@@ -1,14 +1,15 @@
 const { Router } = require('express')
 const {registerRecruiter,loginRecruiter, getDataRecruiter, deleteRecruiter, putRecruiter, patchRecruiter} = require('../controllers/recruiter')
+const {authorization} = require('../middleware/auth')
 
 const router = Router()
 
 
 router.post('/register', registerRecruiter)
 router.post('/login', loginRecruiter)
-router.get('/', getDataRecruiter)
-router.put('/:id', putRecruiter)
-router.delete('/:id', deleteRecruiter)
-router.patch('/:id', patchRecruiter)
+router.get('/',getDataRecruiter)
+router.put('/:id',authorization, putRecruiter)
+router.delete('/:id',authorization, deleteRecruiter)
+router.patch('/:id',authorization, patchRecruiter)
 
 module.exports= router
